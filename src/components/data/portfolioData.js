ecd01d9e7d43bf611641472c7f4941fb6788235f
alfonsovillanueva <alfonso@villanuevagarcia.com>
2025-06-18 00:47:14 -0600

    Add - Blog n 1st Post

--- src/components/data/portfolioData.js
+++ src/components/data/portfolioData.js
@@ -17,66 +17,74 @@
  */
 export const portafolioData = [
 	{
-		imgSrc: 'https://dt2sdf0db8zob.cloudfront.net/wp-content/uploads/2021/06/copy-of-the-6-best-restaurant-website-templates-1.png',
-		title: 'Restaurant',
-		skills: ['React', 'StyledComponents'],
+		imgSrc: '../../../img/villanuevagarcia.webp',
+		title: 'Bienes raíces',
+		skills: ['JavaScript', 'CSS', 'Tailwind'],
 		descripcion:
-			'Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.',
-		demoURL: '',
-		repoURL: '',
+			'Sitio web de Inmobiliaria Villanueva García, realicé Frontend y Backend, actualmente doy mantenimiento y actualización de la web.',
+		demoURL: 'https://www.villanuevagarcia.com/',
 		anim: 'fade-right',
 	},
 	{
-		imgSrc: 'https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/299377097/original/7eb7dcebe244fcf5ad75d92b0969fc116946bd57/create-professional-amd-responsive-wordpress-website.jpg',
-		title: 'E-commerce',
-		skills: ['JavaScript', 'Bootstrap'],
+		imgSrc: '../../img/as.webp',
+		title: 'Arrendamiento Seguro',
+		skills: ['JavaScript', 'CSS'],
 		descripcion:
-			' Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.',
-		demoURL: '',
-		repoURL: '',
+			' Sitio web de Arrendamiento Seguro, realicé Frontend y Backend, actualmente doy mantenimiento y actualización de la web.',
+        demoURL: 'https://www.arrendamientoseguro.app/',
 		anim: 'fade-up',
 		averageBrightness: 0.1,
 	},
 	{
-		imgSrc: 'https://i0.wp.com/themes.svn.wordpress.org/cafe-cafeteria/1.5.3/screenshot.png',
-		title: 'Coffe Store',
-		skills: ['React', 'CSS'],
+		imgSrc: '../../img/cv-esquelete.webp',
+		title: 'CV Digital',
+		skills: ['Astro', 'Tailwind'],
 		descripcion:
-			'Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.',
-		demoURL: '',
-		repoURL: '',
-		anim: 'fade-left',
+			'Un Increible CV que puedes presentar en tus entrevistas!',
+        demoURL: 'https://cv-esquelete.alfgow.app/',
+		anim: 'fade-up',
+		averageBrightness: 0.1,
 	},
-	{
-		imgSrc: 'https://www.corse.mx/cdn/shop/files/tienda_lenceria-lenceria-hot_sale-ofertas-mexico-lenceria_seyx-encaje-corse_mx-corse_lenceria_1200_x_560_px_13.png?v=1712270274&width=1500',
-		title: 'Coffe Store',
-		skills: ['React', 'CSS'],
-		descripcion:
-			'Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.',
-		demoURL: '',
-		repoURL: '',
-		anim: 'fade-left',
-	},
-	{
-		imgSrc: 'https://i0.wp.com/themes.svn.wordpress.org/cafe-cafeteria/1.5.3/screenshot.png',
-		title: 'Coffe Store',
-		skills: ['React', 'CSS'],
-		descripcion:
-			'Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.',
-		demoURL: '',
-		repoURL: '',
-		anim: 'fade-left',
-	},
-	{
-		imgSrc: 'https://dt2sdf0db8zob.cloudfront.net/wp-content/uploads/2021/06/copy-of-the-6-best-restaurant-website-templates-1.png',
-		title: 'Restaurant',
-		skills: ['React', 'StyledComponents'],
-		descripcion:
-			'Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.',
-		demoURL: '',
-		repoURL: '',
-		anim: 'fade-right',
-	}
+	// {
+	// 	imgSrc: 'https://i0.wp.com/themes.svn.wordpress.org/cafe-cafeteria/1.5.3/screenshot.png',
+	// 	title: 'Coffe Store',
+	// 	skills: ['React', 'CSS'],
+	// 	descripcion:
+	// 		'Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.',
+	// 	demoURL: '',
+	// 	repoURL: '',
+	// 	anim: 'fade-left',
+	// },
+	// {
+	// 	imgSrc: 'https://www.corse.mx/cdn/shop/files/tienda_lenceria-lenceria-hot_sale-ofertas-mexico-lenceria_seyx-encaje-corse_mx-corse_lenceria_1200_x_560_px_13.png?v=1712270274&width=1500',
+	// 	title: 'Coffe Store',
+	// 	skills: ['React', 'CSS'],
+	// 	descripcion:
+	// 		'Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.',
+	// 	demoURL: '',
+	// 	repoURL: '',
+	// 	anim: 'fade-left',
+	// },
+	// {
+	// 	imgSrc: 'https://i0.wp.com/themes.svn.wordpress.org/cafe-cafeteria/1.5.3/screenshot.png',
+	// 	title: 'Coffe Store',
+	// 	skills: ['React', 'CSS'],
+	// 	descripcion:
+	// 		'Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.',
+	// 	demoURL: '',
+	// 	repoURL: '',
+	// 	anim: 'fade-left',
+	// },
+	// {
+	// 	imgSrc: 'https://dt2sdf0db8zob.cloudfront.net/wp-content/uploads/2021/06/copy-of-the-6-best-restaurant-website-templates-1.png',
+	// 	title: 'Restaurant',
+	// 	skills: ['React', 'StyledComponents'],
+	// 	descripcion:
+	// 		'Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.',
+	// 	demoURL: '',
+	// 	repoURL: '',
+	// 	anim: 'fade-right',
+	// }
 ];
 
 const skillIcons = {
---
+++ src/content/blog/AWS Well-Architected.md
@@ -0,0 +1,46 @@
+---
+title: 'AWS Well-Architected'
+description: 'Aquí te dejo mis apuntes de AWS Well-Architected'
+pubDate: 'Jun 18 2025'
+heroImage: 'https://alfgow.s3.mx-central-1.amazonaws.com/blogs-alfgow/ChatGPT+Image+18+jun+2025%2C+12_20_34+a.m..png'
+---
+
+![javascript image](https://alfgow.s3.mx-central-1.amazonaws.com/blogs-alfgow/ChatGPT+Image+18+jun+2025%2C+12_20_34+a.m..png)
+
+###
+
+### Hola, en esta entrega te dejaré mis apuntes acerca de AWS Well-Architected
+
+Es un servicio que **ayuda a los arquitectos de la nube a crear una infraestructura segura, de alto rendimiento, resistente y eficiente** para una variedad de aplicaciones y cargas de trabajo. Este marco, creado en torno a seis pilares **(excelencia operativa, seguridad, fiabilidad, eficiencia de rendimiento, optimización de costos y sostenibilidad)**, ofrece un enfoque coherente para que los clientes y los socios evalúen las arquitecturas e implementen diseños escalables.
+
+A continuación trataré de resumir los seis pilares:
+
+### 1 Excelencia operativa
+
+La excelencia operativa (OE) es el **compromiso de crear software correctamente**, al mismo tiempo que se ofrece de forma coherente una excelente experiencia de cliente. El pilar de excelencia operativa **contiene las prácticas recomendadas para organizar su equipo, diseñar su carga de trabajo, ejecutarla a escala y hacerla evolucionar con el tiempo**. El objetivo de la excelencia operativa es **conseguir que las nuevas características y las correcciones de errores lleguen a los clientes de forma rápida y fiable**. Las organizaciones que invierten en excelencia operativa deleitan a los clientes de forma coherente mientras crean nuevas características, hacen cambios y afrontan los errores. Durante el proceso, la excelencia operativa impulsa la integración continua y la entrega continua (CI/CD), lo que ayuda a los desarrolladores a obtener resultados de alta calidad y de forma coherente.
+
+### 2. Seguridad
+
+En el pilar de seguridad, se describe cómo aprovechar las tecnologías en la nube para **proteger los datos, los sistemas y los activos de una manera que pueda mejorar su posición de seguridad**.
+
+### 3. Fiabilidad
+
+El pilar de fiabilidad abarca la **capacidad de una carga de trabajo para llevar a cabo su función prevista de forma correcta y coherente cuando se espera que lo haga**. Esto incluye la capacidad de utilizar y probar la carga de trabajo a lo largo de todo su ciclo de vida.
+
+### Eficiencia del rendimiento
+
+El pilar de eficiencia del rendimiento incluye la **capacidad para utilizar los recursos de la nube de forma eficaz a fin de que satisfagan los requisitos de rendimiento** y para mantener dicha eficacia a medida que la demanda cambia y las tecnologías evolucionan.
+
+### Optimización de costos
+
+La optimización de costos es un proceso continuo de refinamiento y mejora durante el ciclo de vida de una carga de trabajo. Las prácticas de este documento lo ayudan a **crear y operar cargas de trabajo sensibilizadas con los costos que logren resultados empresariales** y, al mismo tiempo, minimicen los costos y permitan a su organización maximizar el retorno de la inversión.
+
+### Sostenibilidad
+
+La sostenibilidad como disciplina **aborda el impacto medioambiental, económico y social a largo plazo de sus actividades empresariales**. La Comisión Mundial sobre el Medio Ambiente y el Desarrollo de las Naciones Unidas define el desarrollo sostenible como “aquel que permite satisfacer las necesidades del presente sin comprometer la habilidad de las futuras generaciones de satisfacer sus necesidades propias”. Su organización o negocio puede tener repercusiones negativas en el medioambiente, como emisiones de carbono directas o indirectas, residuos no reciclables y daños a recursos compartidos, como el agua no contaminada.
+
+### Espero te haya servido éste resumen.
+
+### AV.
+
+![javascript image](<https://alfgow.s3.mx-central-1.amazonaws.com/blogs-alfgow/alfgow+(1).webp>)
